bbe27e90d05e8cdf05b49d05d3cfa3b7ec5f2960
robin <robin@wiillus.com>
2022-08-19 12:33:02 +0900

    email verification

--- pages/user/confirm-email.tsx
+++ pages/user/confirm-email.tsx
@@ -1,6 +1,7 @@
 import { gql, useApolloClient, useMutation } from '@apollo/client';
+import MessageModal from 'components/Modal';
 import { useRouter } from 'next/router';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useMe from '../../utils/hooks/useMe';
 import {
 	VerifyEmail,
@@ -20,7 +21,12 @@ const ConfirmEmail = () => {
 	const { data: userData } = useMe();
 	const client = useApolloClient();
 	const router = useRouter();
-	const onCompleted = useCallback((data: VerifyEmail) => {
+
+	const [modalState, setModalState] = useState(false);
+	const additionalAction = () => {
+		router.push('/');
+	};
+	const onCompleted = (data: VerifyEmail) => {
 		const {
 			verifyEmail: { ok },
 		} = data;
@@ -36,9 +42,9 @@ const ConfirmEmail = () => {
 					verified: true,
 				},
 			});
-			router.push('/');
+			setModalState(true);
 		}
-	}, []);
+	};
 	const [verifyEmail] = useMutation<VerifyEmail, VerifyEmailVariables>(
 		VERIFY_EMAIL_MUTATION,
 		{ onCompleted },
@@ -56,10 +62,19 @@ const ConfirmEmail = () => {
 		}
 	}, [router.query.token]);
 	return (
-		<div className="mt-52 flex flex-col items-center justify-center">
-			<h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
-			<h4 className="text-gray-700 text-sm">잠시만 기다려주십시오.</h4>
-		</div>
+		<>
+			<div className="mt-52 flex flex-col items-center justify-center">
+				<h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
+				<h4 className="text-gray-700 text-sm">잠시만 기다려주십시오.</h4>
+			</div>
+			<MessageModal
+				title="이메일 인증"
+				description="이메일 인증이 완료됐습니다."
+				state={modalState}
+				setState={setModalState}
+				additionalAction={additionalAction}
+			/>
+		</>
 	);
 };
 
