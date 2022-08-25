cc89f80af51ab048673ad76afb9be5fe4a950d95
changmin <developerQuo@gmail.com>
2022-08-25 22:28:48 +0900

    hotfix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,17 +6,17 @@ import KakaoProvider from "next-auth/providers/kakao";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET as string,
     }),
     NaverProvider({
-      clientId: process.env.NAVER_ID as string,
-      clientSecret: process.env.NAVER_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_NAVER_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_NAVER_SECRET as string,
     }),
     KakaoProvider({
-      clientId: process.env.KAKAO_ID as string,
-      clientSecret: process.env.KAKAO_SECRET as string,
-      
+      clientId: process.env.NEXT_PUBLIC_KAKAO_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_KAKAO_SECRET as string,
     }),
   ],
+  secret: process.env.SECRET,
 });
